9045478d67e4771a60ae38dd57df8028ee14fa3b
Michael <michael.morris.london@gmail.com>
2020-05-06 10:46:10 +0100

    updated micropython

--- matlab_sim/rot_z.m
+++ matlab_sim/rot_z.m
@@ -1,6 +1,6 @@
 function pos = rot_z(xyz,theta)
     mat = [cosd(theta),  sind(theta), 0;
-    -sind(theta), cosd(theta), 0;
-    0,0,1];
+           -sind(theta), cosd(theta), 0;
+           0,0,1];
     pos = mat*xyz;
 end
